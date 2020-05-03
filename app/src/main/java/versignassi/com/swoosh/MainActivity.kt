cece796ba65641892f6417f0fbea6a4509864bd4
bruno-vargas <brunoversignassi@gmail.com>
2020-05-03 16:48:49 -0300

    navigation between 2 Activities

--- app/src/main/java/versignassi/com/swoosh/MainActivity.kt
+++ app/src/main/java/versignassi/com/swoosh/MainActivity.kt
@@ -1,12 +1,23 @@
 package versignassi.com.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //viewDidLoad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this, League_activity::class.java)
+            startActivity(leagueIntent)
+        }
+
     }
+
+
 }
