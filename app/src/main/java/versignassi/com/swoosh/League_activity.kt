1c86e1a57a1d23eb97a7fc31985885648da6279a
bruno-vargas <brunoversignassi@gmail.com>
2020-05-03 16:26:19 -0300

    select league activity:  - 4 buttons

---
+++ app/src/main/java/versignassi/com/swoosh/League_activity.kt
@@ -0,0 +1,12 @@
+package versignassi.com.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class League_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league_activity)
+    }
+}
